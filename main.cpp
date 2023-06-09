af49dbf0e91211d2bedb4521ea9ea95e691ee0fd
vinc3w <vinc3w59@gmail.com>
2023-06-09 13:31:58 +0800

    Add disclaimer

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include "./include/order.h"
 
+/**
+*   Disclaimer
+*   This code does not work on Linux OS. Only Windows. Don't knwo about Mac OS thos.
+**/
+
 int main()
 {
     Order{}.getOrders();
